585dce11f4c70fc1cb47af9dbafde5c4d617bdb7
Alisa Filatova <alicefill@bk.ru>
2017-08-31 16:13:38 +0300

    добавлена возможность покупки и продажи артефактов, исправлен баг

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -3,8 +3,11 @@
 (function() {
     var shopElement = document.querySelector('.setup-artifacts-shop');
     var artifactsElement = document.querySelector('.setup-artifacts');
+    var money = document.querySelector('.money__count');
 
     var draggedItem = null;
+
+    // Перенос из магазина в рюкзак
 
     shopElement.addEventListener('dragstart', function(event) {
         if (event.target.tagName.toLowerCase() === 'img') {
@@ -18,8 +21,18 @@
     });
 
     artifactsElement.addEventListener('drop', function(event) {
-        event.target.style.backgroundColor = '';
-        event.target.appendChild(draggedItem);
+        if (draggedItem.classList.contains('bought')) {
+            event.target.style.backgroundColor = '';
+            event.target.appendChild(draggedItem);
+            money.textContent = money.textContent;
+        } else {
+            event.target.style.backgroundColor = '';
+            event.target.appendChild(draggedItem);
+            draggedItem.classList.add('bought');
+            draggedItem.classList.remove('sold');
+
+            money.textContent = money.textContent - draggedItem.getAttribute('data');
+        }
     });
 
     artifactsElement.addEventListener('dragenter', function(event) {
@@ -28,6 +41,44 @@
     });
 
     artifactsElement.addEventListener('dragleave', function(event) {
+        event.target.style.backgroundColor = '';
+        event.preventDefault();
+    });
+
+    // Перенос из рюкзака в магазин
+
+    artifactsElement.addEventListener('dragstart', function(event) {
+        if (event.target.tagName.toLowerCase() === 'img') {
+            draggedItem = event.target;
+            event.dataTransfer.setData('text/plain', event.target.alt);
+        }
+    });
+
+    shopElement.addEventListener('dragover', function(event) {
+        event.preventDefault();
+    });
+
+    shopElement.addEventListener('drop', function(event) {
+        if (draggedItem.classList.contains('sold')) {
+            event.target.style.backgroundColor = '';
+            event.target.appendChild(draggedItem);
+            money.textContent = money.textContent;
+        } else {
+            event.target.style.backgroundColor = '';
+            event.target.appendChild(draggedItem);
+            draggedItem.classList.add('sold');
+            draggedItem.classList.remove('bought');
+        
+            money.textContent = Number(money.textContent) + Number(draggedItem.getAttribute('data'));
+        }
+    });
+
+    shopElement.addEventListener('dragenter', function(event) {
+        event.target.style.backgroundColor = '';
+        event.preventDefault();
+    });
+
+    shopElement.addEventListener('dragleave', function(event) {
         event.target.style.backgroundColor = '';
         event.preventDefault();
     });
