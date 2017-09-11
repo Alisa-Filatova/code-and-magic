87a1dd75cf057ec069f48de55ae526965a9bf9c4
Alisa Filatova <alicefill@bk.ru>
2017-09-11 16:19:59 +0300

    добавлена функция загрузки аватара

---
+++ js/upload-avatar.js
@@ -0,0 +1,25 @@
+var fileChooser = document.querySelector('.upload input[type="file"]');
+var preview = document.querySelector('.setup-user-pic');
+var userIcon = document.querySelector('.setup-open-icon');
+
+var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+fileChooser.addEventListener('change', function() {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function(it) {
+        return fileName.endsWith(it);
+    });
+
+    if (matches) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function() {
+            preview.src = reader.result;
+            userIcon.src = reader.result;
+        });
+
+        reader.readAsDataURL(file);
+    }
+});
